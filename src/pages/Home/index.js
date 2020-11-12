2a754a2003bbde5d0ff7f9cb1425b27793897e59
davijose <davijose40@gmail.com>
2020-11-11 21:56:49 -0300

    placed images in home with grid

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@
 import { Container, Row, Column } from './style';
 import hero_page_01 from '../../assets/hero_page_01.png';
 import hero_page_02 from '../../assets/hero_page_02.png';
-import hero_page_03 from '../../assets/hero_page_02.png';
+import hero_page_03 from '../../assets/hero_page_03.png';
 import hero_side_01 from '../../assets/hero_side_01.png';
 import hero_side_02 from '../../assets/hero_side_02.png';
 
@@ -14,23 +14,23 @@ function Home(){
                 <Row>
 
                     <Column grid='12'>
-                        <img src={hero_page_01}style={{width:'100%'}}alt="Hero page"/>
-                    </Column>
-
-                    <Column grid='3'>
-                       <img src={hero_side_01} alt="Hero page"/>
-                    </Column>
-                    <Column grid='9'>
-                       <img src={hero_page_02} alt="Hero page"/>
-                    </Column>
-
-                    <Column grid='3'>
-                       <img src={hero_side_02} alt="Hero page"/>
-                    </Column>
-                    <Column grid='9'>
-                       <img src={hero_page_03 } alt="Hero page"/>
+                        <img src={hero_page_01} style={{width:'100%'}}alt="Hero page"/>
                     </Column>
                 </Row>
+                <Row style={{display:'flex',flexDirection:'row'}}> 
+                    <Column grid='3'  >
+                       <img src={hero_side_01} style={{width:'100%'}} alt="Hero page"/>
+                       <img src={hero_side_02} style={{width:'100%' }}  alt="Hero page"/>
+                    </Column>
+                   
+                    <Column grid='9'>
+                       <img src={hero_page_02} style={{width:'100%'}} alt="Hero page"/>
+                    </Column>
+                </Row>
+
+                    <Column grid='12'>
+                       <img src={hero_page_03 } style={{width:'100%'}} alt="Hero page"/>
+                    </Column>
             </Container>
         <h4>Footer</h4>
        </>
