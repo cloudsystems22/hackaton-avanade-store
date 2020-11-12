33699df2a69bdfc5b8a59092b4fc9cd972271ea9
cloudsystems22 <cloudsystems22@gmail.com>
2020-11-11 22:01:58 -0300

    Primeiro card de protudos com botão

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,19 @@
-import { CardPrincipal, ImagPrincipal, DetailsProduto } from './style';
+import { CardPrincipal, ImagPrincipal, DetailsProduto, SpanPreco, TituloProduto, ButtonAdd } from './style';
 
 function Card(){
     return (
         <CardPrincipal>
             <ImagPrincipal></ImagPrincipal>
             <DetailsProduto>
-            <h1>Vera bradley <span>R$ 1000,00</span></h1>
-           <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-           Cras ac posuere justo. Ut neque nunc, facilisis sagittis consequat a, venenatis at massa. 
-           Vivamus tincidunt ligula at interdum feugiat. Fusce eu vulputate sapien. 
-           Suspendisse et est nec orci venenatis vehicula ac at tortor. Vestibulum suscipit consequat dictum. 
-           Maecenas id nisi quis purus rutrum vulputate nec quis mauris.
-           </p>
+            <TituloProduto>Vera bradley <SpanPreco>R$ 1000,00</SpanPreco></TituloProduto>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Cras ac posuere justo. Ut neque nunc, facilisis sagittis consequat a, venenatis at massa. 
+            Vivamus tincidunt ligula at interdum feugiat. Fusce eu vulputate sapien. 
+            Suspendisse et est nec orci venenatis vehicula ac at tortor. Vestibulum suscipit consequat dictum. 
+            Maecenas id nisi quis purus rutrum vulputate nec quis mauris.
+            </p>
+            <ButtonAdd>Add ao carrinho</ButtonAdd>
             </DetailsProduto>
           
         </CardPrincipal>
